4b9c0ca80a50420bbacbb19edfb8a04b73871472
LucaPetrescu <45922759+LucaPetrescu@users.noreply.github.com>
2022-03-07 13:36:41 +0200

    Update keys.js

--- helpers/keys.js
+++ helpers/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://Luca:normandysr2@cluster0.ktaeq.mongodb.net/JWT?retryWrites=true&w=majority",
+    "mongodb+srv://Luca:<typeyourpasswordhere>@cluster0.ktaeq.mongodb.net/JWT?retryWrites=true&w=majority",
 };
