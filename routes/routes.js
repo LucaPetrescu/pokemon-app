61c03474451cd39d87a189802b066765b13a7c88
Luca Petrescu <luca6011@gmail.com>
2022-03-15 20:46:23 +0200

    done

--- routes/routes.js
+++ routes/routes.js
@@ -30,12 +30,12 @@ router.get("/populate-database", async (req, res) => {
       }
       response = urlResponse;
     }
-
+    //Push all the pokemons in an array
     for (let i = 0; i < result.length; i++) {
       let pokemon = await axios.get(result[i].url);
       pokemonsWithAllStats.push(pokemon.data);
     }
-
+    //Create the pokemon object and push it into a new array
     for (let i = 0; i < pokemonsWithAllStats.length; i++) {
       let pokemonRes = {
         name: pokemonsWithAllStats[i].name,
@@ -58,7 +58,7 @@ router.get("/populate-database", async (req, res) => {
       };
       finalPokemons.push(pokemonRes);
     }
-
+    //Using mass add for actualizing the database
     await Pokemon.insertMany(finalPokemons);
     console.log(finalPokemons);
     res.send(finalPokemons);
@@ -68,6 +68,7 @@ router.get("/populate-database", async (req, res) => {
   }
 });
 
+//Creating a new pokemon with name, height, weight and abilities
 router.post("/create-pokemon", async (req, res) => {
   const { value, error } = pokemonValidation(req.body);
   try {
@@ -81,6 +82,7 @@ router.post("/create-pokemon", async (req, res) => {
         weight: value.weight,
         abilities: value.abilities,
       });
+      //Save the pokemons in the database
       const newPoke = await newPokemon.save();
       res.status(200).send(newPoke);
     }
@@ -90,6 +92,7 @@ router.post("/create-pokemon", async (req, res) => {
   }
 });
 
+//Update a pokemon according to an id
 router.post("/update-pokemon", async (req, res) => {
   const { id, name, height, weight, abilities } = req.body;
   const { value, error } = pokemonValidation({
@@ -103,6 +106,7 @@ router.post("/update-pokemon", async (req, res) => {
       let err = createError(404, { error: error.details[0].message });
       res.send(err);
     } else {
+      //Find the pokemon via id
       const pokemon = await Pokemon.findOne({ _id: id });
       let newPokemon = {
         $set: {
@@ -112,6 +116,7 @@ router.post("/update-pokemon", async (req, res) => {
           abilities: value.abilities,
         },
       };
+      //Update the pokemon with the newo pokemon object
       let updatedPokemon = await Pokemon.updateOne(pokemon, newPokemon);
       res.status(200).send(newPokemon);
     }
@@ -121,8 +126,10 @@ router.post("/update-pokemon", async (req, res) => {
   }
 });
 
+//Get the pokemon by id
 router.post("/get-pokemon-by-id", async (req, res) => {
   const { id } = req.body;
+  //Search the database by id and find the desired pokemon
   try {
     let searchedPokemon = await Pokemon.findOne({ _id: id });
     res.status(200).send(searchedPokemon);
@@ -132,7 +139,9 @@ router.post("/get-pokemon-by-id", async (req, res) => {
   }
 });
 
+//Delete a pokemon via id
 router.post("/delete-pokemon", async (req, res) => {
+  //Find the desired pokemon via ID
   try {
     await Pokemon.deleteOne({ _id: req.body.id });
     res.send("NO_CONTENT");
@@ -142,6 +151,7 @@ router.post("/delete-pokemon", async (req, res) => {
   }
 });
 
+//Deletes all pokemons in the collection
 router.post("/delete-all-pokemons", async (req, res) => {
   try {
     await Pokemon.remove();
@@ -152,6 +162,7 @@ router.post("/delete-all-pokemons", async (req, res) => {
   }
 });
 
+//Gets all the pokemons in the collection
 router.get("/get-all-pokemons", async (req, res) => {
   try {
     const pokemons = await Pokemon.find();
@@ -161,7 +172,7 @@ router.get("/get-all-pokemons", async (req, res) => {
     res.send(e);
   }
 });
-
+//This function was used as a next() function, but it is not used anymore, but i decided to leave it here
 async function getAllPokemons(req, res) {
   try {
     const pokemon = await Pokemon.find();
